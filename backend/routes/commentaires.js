92e79b3abc8b4ad58571aa0d0568ed6f12ad1ab7
kenuhn <kenuhn@gmail.com>
2022-02-19 14:43:35 +0100

    coutes post protéger

--- backend/routes/commentaires.js
+++ backend/routes/commentaires.js
@@ -3,8 +3,8 @@ const userCtrl = require('../controllers/commentaire');
 const auth = require('../middleware/auth');
 
 router.get('/home/:postId/commentaires', auth, userCtrl.getAllComment)
-router.post('/home/:postId/commentaires',  userCtrl.postcomment)
-router.delete('/commentaires/:commentID', userCtrl.deleteOneComment)
+router.post('/home/:postId/commentaires', auth, userCtrl.postcomment)
+router.delete('/commentaires/:commentID', auth, userCtrl.deleteOneComment)
 
 
 module.exports = router;
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,10 +4,10 @@ const postCtrl = require('../controllers/post');
 const userCtrl = require('../controllers/utilisateur')
 const multerPost = require('../middleware/multer-post');
 
-router.get('/home', multerPost, postCtrl.getAllPost)
-router.post('/home/:id', multerPost, postCtrl.createpost)
-router.delete('/post/:id', multerPost, postCtrl.deleteOnePost)
-router.post('/home/:postId/like', multerPost, postCtrl.likeOnePost )
+router.get('/home', auth, multerPost, postCtrl.getAllPost)
+router.post('/home/:id',auth,  multerPost, postCtrl.createpost)
+router.delete('/post/:id',auth,  multerPost, postCtrl.deleteOnePost)
+router.post('/home/:postId/like',auth,  multerPost, postCtrl.likeOnePost )
 router.get('/home/like', postCtrl.getAllLike )
 
 
